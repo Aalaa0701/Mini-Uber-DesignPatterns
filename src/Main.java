1f35ea459615dc9a4b444147ff767d9da1ce5353
BasmalaNaeem <119793602+BasmalaNaeem@users.noreply.github.com>
2023-08-23 21:05:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-//        Info info = new Info("nour", "7777");
-//        Map<String, Info> users = new HashMap<>();
-//        users.put("nour", info);
-//        Login lo = new Login();
-//        lo.Data();
-//        System.out.println(Login.users);
-        Register re = new Register();
-        re.Data();
+
     }
 }
