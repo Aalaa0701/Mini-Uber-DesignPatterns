c6edfd0048ecc200b73640e16876361b398cb6a2
Aalaa0701 <Alaaalaaashour1507@gmail.com>
2023-08-28 22:19:03 +0300

    set distances map

--- src/DistanceCalculator.java
+++ src/DistanceCalculator.java
@@ -2,35 +2,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DistanceCalculator {
-    public static void main(String[] args) {
-        Map<String, Map<String, Double>> distances = new HashMap<>();
+    Map<String, Map<String, Double>> distances = new HashMap<>();
+    String currentLocation;
+    String destination;
 
-        // Add distances to the HashMap
-        distances.put("Cairo", new HashMap<>());
-        distances.get("Cairo").put("Alex", 80.0);
-        distances.get("Cairo").put("Giza", 50.0);
-        distances.get("Cairo").put("Aswan", 150.0);
+    public DistanceCalculator() {
+        setDistances();
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    void setDistances(){
+        distances.put("cairo", new HashMap<>());
+        distances.get("cairo").put("alex", 80.0);
+        distances.get("cairo").put("giza", 50.0);
+        distances.get("cairo").put("aswan", 150.0);
 
 
-        distances.put("Alex", new HashMap<>());
-        distances.get("Alex").put("Cairo", 80.0);
-        distances.get("Alex").put("Giza", 90.0);
-        distances.get("Alex").put("Aswan", 200.0);
+        distances.put("alex", new HashMap<>());
+        distances.get("alex").put("cairo", 80.0);
+        distances.get("alex").put("giza", 90.0);
+        distances.get("alex").put("aswan", 200.0);
 
 
 
-        distances.put("Aswan", new HashMap<>());
-        distances.get("Aswan").put("Alex", 200.0);
-        distances.get("Aswan").put("Giza", 170.0);
-        distances.get("Aswan").put("Cairo", 150.0);
+        distances.put("aswan", new HashMap<>());
+        distances.get("aswan").put("alex", 200.0);
+        distances.get("aswan").put("giza", 170.0);
+        distances.get("aswan").put("cairo", 150.0);
         // Similarly, add distances for other cities
 
-        distances.put("Giza", new HashMap<>());
-        distances.get("Giza").put("Alex", 90.0);
-        distances.get("Giza").put("Aswan", 170.0);
-        distances.get("Giza").put("Cairo", 50.0);
+        distances.put("giza", new HashMap<>());
+        distances.get("giza").put("alex", 90.0);
+        distances.get("giza").put("aswan", 170.0);
+        distances.get("giza").put("cairo", 50.0);
+
 
     }
+    double getDistance(){
+        double distance = distances.get(currentLocation).get(destination);
+        System.out.println(distance);
+        return distance;
+    }
+
+
 
 
 }
